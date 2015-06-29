39827a49046e5b0924b478769254d14dac530629
Emerson Matson <emersonn@cs.washington.edu>
2015-06-29 09:41:39 +0200

    abstracted out code, added colors :D

--- twitter/analyze_project.py
+++ twitter/analyze_project.py
@@ -1,19 +1,21 @@
+import random
+
 import nltk
 from nltk.tokenize import word_tokenize
-import random
+from colorama import Fore
 
 from ..database import db_session
 
 import models
 
-LOCATIONS = ('Berlin, Germany', 'Seattle, WA')
+LOCATIONS = ('Berlin, Germany', 'Seattle, WA', 'Los Angeles, CA')
 
 # God is sometimes used colloquially and not in a religious context
 CHRISTIAN_KEYWORDS = ('God', 'Bible', 'Jesus', 'Jesus Christ', 'Church')
 MUSLIM_KEYWORDS = ('Koran', "Quran", "Q'uran", "Allah", "Mosque")
 
 def analyze_topic(tweets, place, topic):
-    print("There are " + str(len(tweets)) + " " + topic + " tweets in " + place + ".")
+    print(Fore.BLUE + "There are " + str(len(tweets)) + " " + topic + " tweets in " + place + "." + Fore.RESET)
 
     print("Here are some samples.")
     for tweet in random.sample(tweets, min(10, len(tweets))):
@@ -27,7 +29,7 @@ def analyze_topic(tweets, place, topic):
     freq_dist = nltk.FreqDist(word_list)
     common_words = [word for word in freq_dist.most_common(49) if len(word[0]) > 2]
 
-    print("Here are the most popular words related to " + topic + " and in " + place + ".")
+    print(Fore.BLUE + "Here are the most popular words related to " + topic + " and in " + place + "." + Fore.RESET)
     print(common_words)
 
     # TODO: needs NLTK.download()
@@ -36,40 +38,28 @@ def analyze_topic(tweets, place, topic):
 
     # Analyze sentiment here and also get the important words in the positive and negative
 
+def analyze_keywords(keywords, tag):
+    tweets = {}
+
+    for place in LOCATIONS:
+        tweets[place] = set()
+        for keyword in keywords:
+            query = models.Tweet.query.filter_by(place = place).filter(models.Tweet.text.like("%" + keyword + "%")).all()
+            for tweet in query:
+                tweets[place].add(tweet)
+
+        analyze_topic(tweets[place], place, tag)
+
+    return tweets
+
 # TODO: occurances of each religion, sentiment towards each religion, similar words in each
 # religion
 def analyze_religion(num_tweets):
-
-    # Christian
-    christian_tweets = {}
-    for place in LOCATIONS:
-        christian_tweets[place] = set()
-        for keyword in CHRISTIAN_KEYWORDS:
-            query = models.Tweet.query.filter_by(place = place).filter(models.Tweet.text.like("%" + keyword + "%")).all()
-            for tweet in query:
-                christian_tweets[place].add(tweet)
-
-        analyze_topic(christian_tweets[place], place, "Christian")
-
-    # Muslim
-    muslim_tweets = {}
-
-    # TODO: put this into a different method
-    for place in LOCATIONS:
-        muslim_tweets[place] = set()
-        for keyword in MUSLIM_KEYWORDS:
-            query = models.Tweet.query.filter_by(place = place).filter(models.Tweet.text.like("%" + keyword + "%")).all()
-            for tweet in query:
-                muslim_tweets[place].add(tweet)
-
-        analyze_topic(muslim_tweets[place], place, "Muslim")
-
-
-
-
+    christian_tweets = analyze_keywords(CHRISTIAN_KEYWORDS, "Christian")
+    muslim_tweets = analyze_keywords(MUSLIM_KEYWORDS, "Muslim")
 
 if __name__ == "__main__":
     num_tweets = models.Tweet.query.count()
-    print("There are " + str(num_tweets) + " tweets in the database.")
+    print(Fore.GREEN + "There are " + str(num_tweets) + " tweets in the database." + Fore.RESET)
 
     analyze_religion(num_tweets)
