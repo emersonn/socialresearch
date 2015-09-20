1b2ccd7fa1efdad1e1920d07c7769dd440390bbd
Emerson Matson <emersonn@cs.washington.edu>
2015-09-19 20:42:16 -0700

    first commit

---
+++ twitter/compile.py
@@ -0,0 +1,8 @@
+# Compiles the database based on current analyzed data
+
+if __name__ == "__main__":
+    # Certain words have certain statistics on certain days?
+    #   Grab the words for a certain day and just store the numbers?
+    # Hashtags? Store the hashtag sentiments?
+    # Topics? Store the topics in a different table of what is necessary?
+    pass
