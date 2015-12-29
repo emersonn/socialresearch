533e1c5c936ef6120f7cce363053cc6194ad5bb9
Emerson Matson <emersonn@cs.washington.edu>
2015-12-28 17:03:54 -0800

    cleaned up crawling stream script

--- analysis/crawl_stream.py
+++ analysis/crawl_stream.py
@@ -1,11 +1,24 @@
-# Crawls the Twitter sample stream and stores the found tweets in the database
+"""
+Twitter Stream Collector
+Emerson Matson
+
+Collects tweets from the Twitter sample stream.
+"""
 
 from colorama import Fore
+
 import tweepy
 
-from ..database import db_session
-import models
-from private import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET
+# TODO(Consider separating both of these modules? Too interlinked?)
+from twitter import app
+from twitter import db
+
+from twitter.models import Tweet
+
+CONSUMER_KEY = app.config['CONSUMER_KEY']
+CONSUMER_SECRET = app.config['CONSUMER_SECRET']
+ACCESS_TOKEN = app.config['ACCESS_TOKEN']
+ACCESS_SECRET = app.config['ACCESS_SECRET']
 
 AUTH = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 AUTH.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
@@ -13,41 +26,35 @@ API = tweepy.API(AUTH)
 
 
 class TweetStreamListener(tweepy.StreamListener):
-    """
-    Adds functionality to the Tweepy Stream Listener
-    """
+    """Adds functionality to the Tweepy Stream Listener"""
 
+    # TODO(Consider using PrettyLog for this.)
     def on_connect(self):
-        print("Successfully connected to Twitter streaming API...")
+        print("Successfully connected to Twitter streaming API.")
 
     def on_status(self, status):
-        try:
-            print(
-                Fore.GREEN + status.user.name + ": " +
-                Fore.BLUE + status.text + Fore.RESET
-            )
-        except UnicodeEncodeError:
-            print("Got tweet: " + str(status.id_str) + ".")
+        print(
+            Fore.GREEN + status.user.name + ": " +
+            Fore.BLUE + status.text + Fore.RESET
+        )
 
         # Checks for coordinates and places outside coordinates
         try:
             longitude = status.coordinates['coordinates'][0]
             latitude = status.coordinates['coordinates'][1]
         except TypeError:
-            longitude = 200
-            latitude = 100
+            longitude = None
+            latitude = None
 
         # Checks for the place otherwise it is undefined
         try:
             place = status.place.full_name
-        except TypeError:
-            place = "Undefined"
-        except AttributeError:
+        except (TypeError, AttributeError):
             place = "Undefined"
 
-        # TODO: Add try/except for very long tweets
+        # TODO(Add try/except for very long tweets.)
         try:
-            store_tweet = models.Tweet(
+            store_tweet = Tweet(
                 text=str(status.text.encode('unicode_escape')),
                 user_id=int(status.user.id_str),
                 number=int(status.id_str),
@@ -60,8 +67,8 @@ class TweetStreamListener(tweepy.StreamListener):
                 place=place
             )
 
-            db_session.add(store_tweet)
-            db_session.commit()
+            db.session.add(store_tweet)
+            db.session.commit()
         except UnicodeDecodeError:
             print("Problem storing a unicode...")
 
@@ -83,14 +90,8 @@ if __name__ == '__main__':
     stream_listener = TweetStreamListener()
     stream = tweepy.Stream(auth=API.auth, listener=stream_listener)
 
-    print("Using sample stream...")
+    print("Starting to collect the sample stream.")
 
-    import sqlalchemy
     while 1:
-        try:
-            print(Fore.RED + "Starting Twitter stream..." + Fore.RESET)
-            stream.sample()
-        except UnicodeDecodeError:
-            print("Encountered UnicodeDecode error.")
-        except sqlalchemy.exc.DataError:
-            print("Encountered SQLAlchemy DataError.")
+        print(Fore.RED + "Starting Twitter stream..." + Fore.RESET)
+        stream.sample()
