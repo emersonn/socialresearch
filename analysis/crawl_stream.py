283cc2c2490102ab65967e4a919443470a95b323
Emerson Matson <emersonn@cs.washington.edu>
2015-12-28 17:09:39 -0800

    added logging and cleaned up code

--- analysis/crawl_stream.py
+++ analysis/crawl_stream.py
@@ -9,6 +9,8 @@ from colorama import Fore
 
 import tweepy
 
+from prettylog import PrettyLog
+
 # TODO(Consider separating both of these modules? Too interlinked?)
 from twitter import app
 from twitter import db
@@ -24,16 +26,18 @@ AUTH = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 AUTH.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 API = tweepy.API(AUTH)
 
+LOGGING = PrettyLog()
+
 
 class TweetStreamListener(tweepy.StreamListener):
     """Adds functionality to the Tweepy Stream Listener"""
 
     # TODO(Consider using PrettyLog for this.)
     def on_connect(self):
-        print("Successfully connected to Twitter streaming API.")
+        LOGGING.push("Successfully connected to Twitter streaming API.")
 
     def on_status(self, status):
-        print(
+        LOGGING.push(
             Fore.GREEN + status.user.name + ": " +
             Fore.BLUE + status.text + Fore.RESET
         )
@@ -52,38 +56,33 @@ class TweetStreamListener(tweepy.StreamListener):
         except (TypeError, AttributeError):
             place = "Undefined"
 
-        # TODO(Add try/except for very long tweets.)
-        try:
-            store_tweet = Tweet(
-                text=str(status.text.encode('unicode_escape')),
-                user_id=int(status.user.id_str),
-                number=int(status.id_str),
-                created_at=status.created_at,
-                favorite_count=status.favorite_count,
-                retweet_count=status.retweet_count,
-                screen_name=status.user.screen_name,
-                longitude=longitude,
-                latitude=latitude,
-                place=place
-            )
+        store_tweet = Tweet(
+            text=str(status.text.encode('unicode_escape')),
+            user_id=int(status.user.id_str),
+            number=int(status.id_str),
+            created_at=status.created_at,
+            favorite_count=status.favorite_count,
+            retweet_count=status.retweet_count,
+            screen_name=status.user.screen_name,
+            longitude=longitude,
+            latitude=latitude,
+            place=place
+        )
 
-            db.session.add(store_tweet)
-            db.session.commit()
-        except UnicodeDecodeError:
-            print("Problem storing a unicode...")
+        db.session.add(store_tweet)
+        db.session.commit()
 
     def on_error(self, status_code):
-        if status_code == 420:
-            print("Received 420. KKona.")
-        else:
-            print(
-                "Received error with status code: ", str(status_code) +
-                ". Disconnecting..."
-            )
+        print(
+            "Received error with status code: ", str(status_code) +
+            ". Disconnecting from stream."
+        )
+
         return False
 
     def on_timeout(self):
-        print("Server timed out...")
+        print("Server timed out.")
+
         return True
 
 if __name__ == '__main__':
@@ -92,6 +91,5 @@ if __name__ == '__main__':
 
     print("Starting to collect the sample stream.")
 
-    while 1:
-        print(Fore.RED + "Starting Twitter stream..." + Fore.RESET)
-        stream.sample()
+    print(Fore.RED + "Starting Twitter stream..." + Fore.RESET)
+    stream.sample()
